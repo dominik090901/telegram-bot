8453f33ffb235dc87e81ecfbb90e5424255b6355
dominik090901 <niskiezawieszenie123@gmail.com>
2025-07-10 15:34:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,17 @@
 import logging
 from telegram.ext import Application, CommandHandler
 
-# Funkcja na komendę /start
 async def start(update, context):
     await update.message.reply_text("Bot działa!")
 
-# Ustawienia logowania
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
 
-# Wklej swój TOKEN z BotFather
-TOKEN = "TU_WKLEJ_TOKEN"
+TOKEN = "7280780498:AAFUnTebOpiqv0_jz-EIEVzdOQvLsLLEXvE"
 
-# Tworzymy aplikację i handler
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-# Bot działa w pętli 24/7
 app.run_polling()
